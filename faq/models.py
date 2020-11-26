5f303c41926e293721443b7fa6eea62df1ac4455
chldbwjd70 <chldbwjd02@naver.com>
2020-11-26 17:10:28 +0900

    put

--- faq/models.py
+++ faq/models.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 
 
 class Faq(models.Model):
-    title = models.CharField(max_length=120, verbose_name='제목')
+    title = models.CharField(max_length=200,verbose_name='제목')
     contents = models.TextField(verbose_name='내용')
     registered_time = models.DateTimeField(auto_now_add=True, verbose_name='등록시간')
 
